16c2236af7172b63c0bbdf7b66f3e71bf1420f95
Emmay <emmayalam1@gmail.com>
2021-11-01 09:56:22 -0400

    added session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,43 @@
+import * as SessionAPIUtil from '../util/session';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user
+});
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
+
+const receiveSessionErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
+export const registerUser = user => dispatch => (
+  SessionAPIUtil.register(user)
+    .then(
+      user => dispatch(receiveCurrentUser(user)),
+      errors => dispatch(receiveSessionErrors(errors.responseJSON))
+    )
+)
+
+export const loginUser = user => dispatch => (
+  SessionAPIUtil.login(user)
+    .then(
+      user => dispatch(receiveCurrentUser(user)),
+      errors => dispatch(receiveSessionErrors(errors.responseJSON))
+    )
+)
+
+export const logoutUser = user => dispatch => (
+  SessionAPIUtil.logout(user)
+    .then(
+      () => dispatch(logoutCurrentUser()),
+      errors => dispatch(receiveSessionErrors(errors.responseJSON))
+    )
+)
