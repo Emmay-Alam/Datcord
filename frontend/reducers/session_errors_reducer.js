b45b3ee35c943cc28d57ef69c42f743271c93746
Emmay <emmayalam1@gmail.com>
2021-11-01 10:30:31 -0400

    added user auth reducers

---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,18 @@
+import {
+  RECEIVE_CURRENT_USER,
+  RECIEVE_SESSION_ERRORS } from '../actions/session_actions';
+
+const SessionErrorsReducer = (state = [], action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECIEVE_SESSION_ERRORS:
+      return action.errors;
+    case RECEIVE_CURRENT_USER:
+      return [];
+    default:
+      return state;
+    }
+}
+
+export default SessionErrorsReducer;
---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,20 @@
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT_CURRENT_USER } from '../actions/session_actions';
+
+const _nullSession = { id: null }
+
+const SessionReducer = (oldstate = _nullSession, action) => {
+  Object.freeze(oldstate);
+
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return { id: action.currentUser.id };
+    case LOGOUT_CURRENT_USER:
+      return _nullSession;
+    default:
+      return oldstate;
+  }
+}
+
+export default SessionReducer;
