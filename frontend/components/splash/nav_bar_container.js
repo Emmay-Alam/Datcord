8adb150863e926f00eb23c2524de84bc6075ebe2
Emmay <emmayalam1@gmail.com>
2021-11-01 22:39:53 -0400

    fixed logoutUser bug

--- frontend/components/splash/nav_bar_container.js
+++ frontend/components/splash/nav_bar_container.js
@@ -1,13 +1,13 @@
 import { connect } from 'react-redux';
 import NavBar from './nav_bar';
-import { logout } from '../../actions/session_actions';
+import { logoutUser } from '../../actions/session_actions';
 
 const mSTP = state => ({
   user: state.session.id
 });
 
 const mDTP = dispatch => ({
-  logout: () => dispatch(logout())
+  logoutUser: () => dispatch(logoutUser())
 });
 
 export default connect(mSTP, mDTP)(NavBar);
