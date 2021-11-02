6fff88dd7cbee1932ee9ddad642a29e2c2fc7e4c
Emmay <emmayalam1@gmail.com>
2021-11-01 22:36:59 -0400

    added reset errors action

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -3,6 +3,7 @@ import * as SessionAPIUtil from '../util/session';
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+export const RESET_SESSION_ERRORS = 'RESET_SESSION_ERRORS';
 
 const receiveCurrentUser = user => ({
   type: RECEIVE_CURRENT_USER,
@@ -17,6 +18,10 @@ const receiveSessionErrors = errors => ({
   type: RECEIVE_SESSION_ERRORS,
   errors
 });
+
+const resetSessionErrors = () => ({
+  type: RESET_SESSION_ERRORS
+})
 
 export const registerUser = user => dispatch => (
   SessionAPIUtil.register(user)
