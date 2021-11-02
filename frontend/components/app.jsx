b86beac8b01f4e42a0072dfd36dca91f066e423e
Emmay <emmayalam1@gmail.com>
2021-11-01 21:15:52 -0400

    updated splash page with minor css

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import { AuthRoute, ProtectedRoute } from '../util/routes';
-import NavBarContainer from './splash/nav_bar_container';
+import Splash from './splash/splash';
 import RegisterContainer from './session/register_container';
 import LoginContainer from './session/login_container';
 import Me from './main/me';
@@ -9,7 +9,7 @@ import Me from './main/me';
 const App = () => (
   <div>
     <Switch>
-      <Route exact path="/" component={NavBarContainer} />
+      <Route exact path="/" component={Splash} />
       <AuthRoute exact path='/register' component={RegisterContainer} />
       <AuthRoute exact path='/login' component={LoginContainer} />
       <ProtectedRoute exact path='/@me' component={Me} />
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -6,7 +6,7 @@ class Splash extends React.Component {
     return (
       <main>
         <div className="splash-header">
-          <div className="navbar-container">
+          <div className="navbar-header">
             <ul className="navbar-links">
               <li>Github</li>
               <li>LinkedIn</li>
@@ -15,8 +15,14 @@ class Splash extends React.Component {
             </ul>
             <NavBarContainer />
           </div>
+          <div className="splash-slogan">
+            <h1>IMAGINE A PLACE...</h1>
+            <p>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
+          </div>
         </div>
       </main>
     )
   }
 }
+
+export default Splash;
