771c47bf43fc17dc226b15511d7cf20dd4215268
Emmay <emmayalam1@gmail.com>
2021-11-02 21:41:24 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, defaults: {format: :json} do
+  namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
+
+    resources :servers, only: [:index, :show, :create, :update, :destroy] do
+      resources :channels, only: [:index, :show, :create]
+    end
+
+    resources :channels, only: [:update, :destroy]
   end
 
   root to: 'static_pages#root'
